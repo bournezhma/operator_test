8350db1440cb9a58b382aa61617346f72dcd463d
bourne.zhihuang.ma <bourne.zhihuang.ma@gmail.com>
2022-11-17 19:33:09 +0800

    add controller logic

--- 11/pkg/controller.go
+++ 11/pkg/controller.go
@@ -1,9 +1,13 @@
 package pkg
 
 import (
+	"context"
+	v1 "k8s.io/api/core/v1"
 	net "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	api "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 	coreInformer "k8s.io/client-go/informers/core/v1"
 	netInformer "k8s.io/client-go/informers/networking/v1"
 	"k8s.io/client-go/kubernetes"
@@ -12,6 +16,12 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 	"reflect"
+	"time"
+)
+
+const (
+	workNum  = 5
+	maxRetry = 10
 )
 
 type controller struct {
@@ -33,7 +43,7 @@ func (c *controller) addService(obj interface{}) {
 	c.enqueue(obj)
 }
 
-// 只需要入队一个key就行，worker可以根据key去Indexer里查找obj
+// 只需要入队一个 key 就行，worker 可以根据 key 去 Indexer 里查找 obj
 func (c *controller) enqueue(obj interface{}) {
 	key, err := cache.MetaNamespaceKeyFunc(obj)
 	if err != nil {
@@ -45,11 +55,11 @@ func (c *controller) enqueue(obj interface{}) {
 
 func (c *controller) deleteIngress(obj interface{}) {
 	ingress := obj.(*net.Ingress)
-	service := api.GetControllerOf(ingress)
-	if service != nil {
+	ownerReference := api.GetControllerOf(ingress)
+	if ownerReference == nil {
 		return
 	}
-	if service.Kind != "Service" {
+	if ownerReference.Kind != "Service" {
 		return
 	}
 
@@ -57,7 +67,120 @@ func (c *controller) deleteIngress(obj interface{}) {
 }
 
 func (c *controller) Run(stopCh chan struct{}) {
+	for i := 0; i < workNum; i++ {
+		go wait.Until(c.worker, time.Minute, stopCh) // 保证一直有 workNum 个 worker 在线
+	}
+
 	<-stopCh
+}
+
+func (c *controller) worker() {
+	for c.processNextItem() {
+
+	}
+}
+
+func (c *controller) processNextItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	defer c.queue.Done(item)
+
+	key := item.(string)
+	err := c.syncService(key)
+	if err != nil {
+		c.handleError(key, err)
+	}
+	return true
+}
+
+func (c *controller) syncService(key string) error {
+	namespaceKey, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return err
+	}
+
+	// service 已被其自带的控制器删除，这里只需要根据 service 创建或删除 ingress 即可
+	service, err := c.serviceLister.Services(namespaceKey).Get(name)
+	if errors.IsNotFound(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	// 新增和删除 ingress
+	_, ok := service.GetAnnotations()["ingress/http"]
+	ingress, err := c.ingressLister.Ingresses(namespaceKey).Get(name)
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	if ok && errors.IsNotFound(err) {
+		// create ingress
+		ig := c.constructIngress(service)
+		_, err := c.client.NetworkingV1().Ingresses(namespaceKey).Create(context.TODO(), ig, api.CreateOptions{})
+		if err != nil {
+			return err
+		}
+	} else if !ok && ingress != nil {
+		// delete ingress
+		err := c.client.NetworkingV1().Ingresses(namespaceKey).Delete(context.TODO(), name, api.DeleteOptions{})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *controller) handleError(key string, err error) {
+	if c.queue.NumRequeues(key) <= maxRetry {
+		c.queue.AddRateLimited(key) // 重新入队
+		return
+	}
+
+	runtime.HandleError(err)
+	c.queue.Forget(key)
+}
+
+func (c *controller) constructIngress(service *v1.Service) *net.Ingress {
+	ingress := net.Ingress{}
+	ingress.ObjectMeta.OwnerReferences = []api.OwnerReference{
+		*api.NewControllerRef(service, v1.SchemeGroupVersion.WithKind("Service")),
+	}
+	ingress.Name = service.Name
+	ingress.Namespace = service.Namespace
+	pathType := net.PathTypePrefix
+	icn := "nginx" // ingressClassName
+	ingress.Spec = net.IngressSpec{
+		IngressClassName: &icn,
+		Rules: []net.IngressRule{
+			{
+				Host: "example.com",
+				IngressRuleValue: net.IngressRuleValue{
+					HTTP: &net.HTTPIngressRuleValue{
+						Paths: []net.HTTPIngressPath{
+							{
+								Path:     "/",
+								PathType: &pathType,
+								Backend: net.IngressBackend{
+									Service: &net.IngressServiceBackend{
+										Name: service.Name,
+										Port: net.ServiceBackendPort{
+											Number: 80,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return &ingress
 }
 
 func NewController(client kubernetes.Interface, serviceInformer coreInformer.ServiceInformer,
